91e47d35cd4961bba3759090fe63aa7cf49aab7a
Amanda Barrafato <amb1047@nyu.edu>
2021-03-15 10:56:22 -0400

    DB fix

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,27 @@
 const Sequelize = require("sequelize");
+
+let config;
+
+if (process.env.DATABASE_URL) {
+	config = {
+		logging: false,
+		ssl: true,
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
+	};
+} else {
+	config = {
+		logging: false,
+	};
+}
+
 const db = new Sequelize(
 	process.env.DATABASE_URL || "postgres://localhost:5432/synth",
-	{
-		logging: false,
-	}
+	config
 );
 
 module.exports = {
